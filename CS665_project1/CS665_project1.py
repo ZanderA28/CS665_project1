8033a2eb4c244fb1597d9c7916dd9aa81120b54a
Alexander Asmussen <c697t652@wichita.edu>
2025-04-05 19:39:20 -0500

    Add project files.

---
+++ CS665_project1/CS665_project1.py
@@ -0,0 +1,15 @@
+import tkinter
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",     # or your DB host
+    user=input("Username: "),
+    password=input("Password: "),
+    database="alexsaircraft" 
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM aircraft")
+for row in cursor.fetchall():
+    print(row)
